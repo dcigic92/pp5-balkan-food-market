4623a2da383287e808a71f497cf0699b9c451bed
dcigic92 <dinocigic@yahoo.com>
2024-04-03 21:51:44 +0100

    Add contexts and shopping cart template

--- balkan_food_market/settings.py
+++ balkan_food_market/settings.py
@@ -154,3 +154,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 30
+STANDARD_DELIVERY = 6
