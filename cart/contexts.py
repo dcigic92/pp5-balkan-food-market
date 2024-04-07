6a22647cd389043b3a304cc7ac7a19ab0ea5cc05
dcigic92 <dinocigic@yahoo.com>
2024-04-07 15:59:01 +0100

    Create checkout app and models

--- cart/contexts.py
+++ cart/contexts.py
@@ -5,13 +5,13 @@ from products.models import Product
 def cart_contents(request):
 
     cart_items = []
-    total = 0
+    cart_cost = 0
     product_count = 0
     cart = request.session.get('cart', {})
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        cart_cost += quantity * product.price
         product_count += quantity
         cart_items.append({
             'item_id': item_id,
@@ -19,20 +19,20 @@ def cart_contents(request):
             'product': product,
         })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = settings.STANDARD_DELIVERY
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    if cart_cost < settings.FREE_DELIVERY_THRESHOLD:
+        delivery_cost = settings.STANDARD_DELIVERY
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - cart_cost
     else:
-        delivery = 0
+        delivery_cost = 0
         free_delivery_delta = 0
     
-    total_cost = delivery + total
+    total_cost = delivery_cost + cart_cost
     
     context = {
         'cart_items': cart_items,
-        'total': total,
+        'cart_cost': cart_cost,
         'product_count': product_count,
-        'delivery': delivery,
+        'delivery_cost': delivery_cost,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'total_cost': total_cost,
