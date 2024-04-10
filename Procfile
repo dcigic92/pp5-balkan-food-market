01867f1f654437a3a13b0afdd4ef6035d016e62c
dcigic92 <dinocigic@yahoo.com>
2024-04-10 22:47:43 +0100

    Deployement to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn balkan_food_market.wsgi:application
--- balkan_food_market/settings.py
+++ balkan_food_market/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
@@ -117,11 +118,15 @@ WSGI_APPLICATION = 'balkan_food_market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+ # DATABASES = {
+ #     'default': {
+ #         'ENGINE': 'django.db.backends.sqlite3',
+ #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #     }
+ # }
+     
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
