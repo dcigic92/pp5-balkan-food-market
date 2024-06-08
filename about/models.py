3b1984f892ccfef5c191bc6568b46d3d7b909b12
dcigic92 <dinocigic@yahoo.com>
2024-06-08 17:41:55 +0100

    Add faqs model, view and template

--- about/models.py
+++ about/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Faqs(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "FAQs"
+
+    def __str__(self):
+        return self.question
--- about/views.py
+++ about/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Faqs
 
 
 def about(request):
     """ A view to return the index page """
 
     return render(request, 'about/about.html')
+
+
+def faqs(request):
+
+    context = {
+        'faqs': Faqs.objects.all(),
+    }
+
+    return render(request, 'about/faqs.html', context)
